1bee35b51259e2d9e0bfc526b8b1b227c098ab65
Fred Cox <mcfedr@gmail.com>
2019-02-19 10:02:01 +0200

    Add form type guesser

---
+++ tests/Mcfedr/UuidExtraBundle/Form/Model/Foo.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mcfedr\UuidExtraBundle\Form\Model;
+
+use Ramsey\Uuid\UuidInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Foo
+{
+    /**
+     * @var ?UuidInterface
+     *
+     * @Assert\Uuid
+     */
+    private $uuid;
+
+    public function getUuid(): ?UuidInterface
+    {
+        return $this->uuid;
+    }
+
+    public function setUuid(?UuidInterface $uuid): void
+    {
+        $this->uuid = $uuid;
+    }
+}
---
+++ tests/Mcfedr/UuidExtraBundle/Tests/Form/Type/FooTypeTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mcfedr\UuidExtraBundle\Tests\Form\Type;
+
+use Mcfedr\UuidExtraBundle\Form\Model\Foo;
+use Mcfedr\UuidExtraBundle\Form\Type\FooType;
+use Mcfedr\UuidExtraBundle\Form\Type\UuidType;
+use Ramsey\Uuid\UuidInterface;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class FooTypeTest extends WebTestCase
+{
+    public function testFoo()
+    {
+        $client = self::createClient();
+
+        $f = new Foo();
+
+        $form = $client->getContainer()->get('form.factory')
+            ->createBuilder(FooType::class, $f)
+            ->getForm();
+
+        $element = $form->get('uuid');
+        $this->assertInstanceOf(UuidType::class, $element->getConfig()->getType());
+
+        $request = Request::create('/', 'POST', [
+            'foo' => [
+                'uuid' => '5b10f27c-8a8f-4cd4-9d0b-1ff2f8d7a268',
+            ],
+        ]);
+
+        $form->handleRequest($request);
+        $this->assertInstanceOf(UuidInterface::class, $f->getUuid());
+    }
+}
